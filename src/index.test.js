c4ec191cc6a8c3c10caa82b09847e7569edcfd30
Duran Cristhian <durancristhian@gmail.com>
2015-10-29 15:38:05 -0300

    Add tests to the library

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,35 @@
 var expect = require("chai").expect;
+var names = require("./index");
 
 describe("Los Simuladores", function () {
 
-	it("should work!", function () {
+	describe("all", function () {
 
-		expect(true).to.be.true;
+		it("should be an array of strings", function () {
+
+			expect(names.all).to.satisfy(isArrayOfStrings);
+
+			function isArrayOfStrings (array) {
+
+				return array.every(function (item) {
+
+					return typeof item === "string";
+				});
+			}
+		});
+
+		it("should countain 'Mario Santos'", function () {
+
+			expect(names.all).to.include("Mario Santos");
+		});
+	});
+
+	describe("random", function () {
+
+		it("should return a random item from names.all", function () {
+
+			var randomItem = names.random();
+			expect(names.all).to.contain(randomItem);
+		});
 	});
 });
