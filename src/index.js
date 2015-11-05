ffc3ee1f0cfb0e257889a56819ffdcf9eecdc662
Duran Cristhian <durancristhian@gmail.com>
2015-11-05 16:33:49 -0300

    feat(random): Refactoring

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@ import names from "./names.json";
 import uniqueRandomArray from "unique-random-array";
 var getRandomItem = uniqueRandomArray(names);
 
-function random (number) {
-
-	if(number === undefined) {
-
-		return getRandomItem();
-	} else {
-
-		var randomItems = [];
-		for (var i = 0; i < number; i++) {
-
-			randomItems.push(getRandomItem());
-		};
-
-		return randomItems;
-	}
-}
-
 module.exports = {
 	all: names,
-	random: random
+	random: function (number) {
+
+		if(number === undefined) {
+
+			return getRandomItem();
+		} else {
+
+			var randomItems = [];
+			for (var i = 0; i < number; i++) {
+
+				randomItems.push(getRandomItem());
+			};
+
+			return randomItems;
+		}
+	}
 };
